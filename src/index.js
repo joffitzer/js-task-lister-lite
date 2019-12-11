d1764de4e08e4d6735b215d4983b69e7832388f0
joffitzer <joffitzer@gmail.com>
2019-12-11 09:25:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form)
+  const userInput = formData.get("new-task-description")
+  const newLi = document.createElement("li")
+  newLi.innerText = userInput
+  const deleteButton = document.createElement("BUTTON")
+  deleteButton.innerText = "X"
+  deleteButton.className = "deletebutton"
+  newLi.appendChild(deleteButton)
+  newLi.querySelector("button").addEventListener("click", function (e) {
+    e.currentTarget.parentNode.remove()
+  })
+  const ul = document.querySelector('ul')
+  ul.appendChild(newLi)
+})
 });
